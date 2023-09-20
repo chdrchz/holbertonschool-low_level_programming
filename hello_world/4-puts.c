4097a80884232f12befb3aff5ee29435fce97a61
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-20 09:20:49 -0700

    testing betty linter

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * puts - prints a specific line
  *
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
