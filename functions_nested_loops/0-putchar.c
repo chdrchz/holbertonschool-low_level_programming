b341c468370490c31ece9836e576ad752eb644a2
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-21 09:01:40 -0700

    testing printing _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - what does this do?
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	putchar('\n');
+
+	return (0);
+}
