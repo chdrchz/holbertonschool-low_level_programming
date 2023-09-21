a136e1bd760009879e1340391361b653e7e3b7bc
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-20 19:55:08 -0700

    testing using putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - using putchar function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x// x is my placeholder for every letter in the alphabet
+
+	for(int x = 'a'; x <= 'z'; x++)
+		putchar(x);
+		putchar('\n');
+	return(0);
+}
