226b0abb794d60f520e2456e4185a517df7ef313
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-20 19:56:27 -0700

    test

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,11 +2,12 @@
 /**
  * main - using putchar function
  *
+ * x is my placeholder for every letter in the alphabet
  * Return: 0
  */
 int main(void)
 {
-	char x// x is my placeholder for every letter in the alphabet
+	char x
 
 	for(int x = 'a'; x <= 'z'; x++)
 		putchar(x);
