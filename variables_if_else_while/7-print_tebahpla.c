bbe647fb528166ef94bebf4ff3fd92d93867e15c
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-20 21:51:03 -0700

    testing printing the alphabet backwards using putchar

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - what does this do?
+ *
+ * Return: ?
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
