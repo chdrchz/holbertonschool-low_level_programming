62e176436557249ab93130711d3502c06a2572cd
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-21 15:20:00 -0700

    testing

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,14 +9,11 @@ int main(void)
 {
 	_putchar('_');
 	_putchar('p');
-	_putchar('r');
-	_putchar('i');
-	_putchar('n');
+	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r');
 	_putchar('r');
 	return (0);
 }
