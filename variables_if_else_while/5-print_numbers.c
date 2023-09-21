71f35ce09f262f8c701876a6481803b9eb15d4bc
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-20 21:14:11 -0700

    writing a function that prints all base 10 numbers followed by a new line

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - what does this do?
+ *
+ * Return: ?
+ */
+int main(void)
+{
+	int x;
+	
+	for (x = 0; x <= 9; ++x)
+	{
+		printf("%d\n", x);
+	}
+
+	return (0);
+}
