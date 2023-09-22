755865446c91447a027a3b4f38f9a9a253009a65
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-22 10:55:19 -0700

    fixing betty errors

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _isalpha - tests if a char is in the alphabet
- *
+ * @x: variable for iterating through the alphabet
  * Return: 1 and 0
  */
 int _isalpha(int x)
 {
-	if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))	
+	if ((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))
 		return (1);
 	else
 		return (0);
