d9dc676a4e4b3abc82b520b40038845a3d24a323
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-22 07:49:46 -0700

    testing printing alphabet putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - this will print the alphabet using _putchar
+ *
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++);
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+
+	return (0);
+}
