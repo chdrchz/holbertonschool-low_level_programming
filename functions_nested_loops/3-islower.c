cf437940219e029449f38ecff9cdd755cb3f4ab3
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-22 09:46:14 -0700

    testing if the letter c is a lowercase character

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "mian.h"
+/**
+ * int _islower - description
+ *
+ * Return: ?
+ */
+int _islower(int x)
+{
+	int x;
+
+	if (x >= 'a' && x <=z 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
