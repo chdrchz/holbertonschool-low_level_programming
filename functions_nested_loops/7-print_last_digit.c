be4712547ae7a5c2f5bf04f29d25ad29c48eb2e0
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-22 11:55:01 -0700

    testing compiling

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - what does this do?
+ * print_last_digit - print last digit twice
  *
- * Return: ?
+ * Return: x % 10
  */
 int print_last_digit(int x)
 {
 	if ((x % 10) < 0)
-		n *= -1;
+		x *= -1;
 	_putchar((x % 10) + '0')
 		return (x % 10);
 }
