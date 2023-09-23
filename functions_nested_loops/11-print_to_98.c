cb0f2dc6cef046c867bc90d39e15a8a62b965959
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-22 20:59:44 -0700

    testing print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+ * print_to_98 - prints all natural numbers from a starting number
+ *
+ * Return: all natural numbers
+ */
+void print_to_98(int x)
+{
+	if (x >=98)
+	{
+		while (n > 98)
+		{
+			printf("%d ", n--);
+		}
+		printf("%d /n", n);
+	}
+	else (x <=98)
+	{ 
+		while (n < 98) 
+		{ 
+			printf("%d ", n++); 
+		} 
+		printf("%d /n", n); 
+	}
+}
