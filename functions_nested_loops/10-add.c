0859a7f8821b8097c5a36ec6a5f6b22b4627214c
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-22 19:03:43 -0700

    testing script to remove trailing whitespace

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add - adds two integers together
  * @x: variable for addition
- * @n: variable for addition 
+ * @n: variable for addition
  ** Return: the addition of int x and int n
  */
 int add(int x, int n)
