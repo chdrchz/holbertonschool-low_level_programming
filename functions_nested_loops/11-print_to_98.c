a6415d13a8ad7b3159b01a298f1f8c3df75b509e
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-22 21:12:24 -0700

    fixing errors

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * print_to_98 - prints all natural numbers from a starting number
- *
+ *@x: input integer
  * Return: all natural numbers
  */
 void print_to_98(int x)
 {
-	if (x >=98)
+	if (x >= 98)
 	{
 		while (x > 98)
 		{
