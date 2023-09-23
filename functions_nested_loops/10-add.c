7942fde64d91f9e5738fb6dd66c84f947b5fd348
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-22 18:46:46 -0700

    testing format for variable documentation

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * add - adds two integers together
- * @x n: two variables for addition
- * Return: the addition of int x and int n
+ * @x: variable for addition
+ * @n: variable for addition 
+ ** Return: the addition of int x and int n
  */
 int add(int x, int n)
 {
