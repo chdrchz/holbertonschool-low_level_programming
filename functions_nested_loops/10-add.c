dbf81a31209b3e1b4b2a541a4a2497f3e9df2dde
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-22 18:42:39 -0700

    testing variable descriptions for betty

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * add - adds two integers together
- * @x, n: two variables for addition
+ * @x n: two variables for addition
  * Return: the addition of int x and int n
  */
 int add(int x, int n)
