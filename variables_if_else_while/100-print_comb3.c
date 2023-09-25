01d5274eadf29076d0765333f85699fe43359f48
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-25 08:30:50 -0700

    fixing spacing for betty

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 	int x;
 	int y;
 
-	for(x = 0; x < 9; x++)
+	for (x = 0; x < 9; x++)
 	{
-		for(y = x + 1; y < 10; y++)
+		for (y = x + 1; y < 10; y++)
 		{
 			putchar((x % 10) + '0');
 			putchar((y % 10) + '0');
 
-			if(x == 8 && y == 9)
+			if (x == 8 && y == 9)
 				continue;
 
 			putchar(',');
