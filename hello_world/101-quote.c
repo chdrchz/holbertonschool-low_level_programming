736bd5e6669a50e9d8ac39895b2b5319436d141d
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-25 07:33:06 -0700

    removing whitespace and adding documentation

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - prints a specific line to standard error
+ *
+ * Return: 1
+ */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
