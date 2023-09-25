11d60c2b23f9f94628327ca60167f1df92b25271
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-25 08:25:27 -0700

    testing loop to print all first instances of two numbers

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,23 @@
 int main(void)
 {
 	int x;
+	int y;
 
 	for(x = 0; x < 9; x++)
 	{
-		if (x < 9)
-			putchar((x / 10) + '0');
+		for(y = x + 1; y < 10; y++)
+		{
 			putchar((x % 10) + '0');
-	}	
+			putchar((y % 10) + '0');
+
+			if(x == 8 && y == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');	
 
 	return (0);
 }
