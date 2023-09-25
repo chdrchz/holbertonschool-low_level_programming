f3915e3fdd76f495fbab7db7c0cafbd6997fcaa8
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-25 09:15:12 -0700

    testing adding spaces

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -13,9 +13,11 @@ int main(void)
 		for (y = x + 1; y <= 99; y++)
 		{
 			putchar((x / 10) + '0');
+			putchar(' ');
 			putchar((x % 10) + '0');
 			putchar(' ');
 			putchar((y / 10) + '0');
+			putchar(' ');
 			putchar((y % 10) + '0');
 
 			if (x == 98 && y == 99)
