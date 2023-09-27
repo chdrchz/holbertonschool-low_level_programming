e1992f6409ad11a88c8e41fa0016c30f836ac943
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-27 10:16:37 -0700

    testing

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,10 +9,8 @@ void print_diagonal(int n)
 	int b;
 	int s;
 	
-	if (n < 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
 
 	for (b = 0; b < n; b++)
 	{
@@ -26,6 +24,7 @@ void print_diagonal(int n)
 		
 			continue;
 		_putchar('\n');
+	}
 	}
 	_putchar('\n');
 }
