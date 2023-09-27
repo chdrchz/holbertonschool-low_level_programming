d0c1bed590265a5d73ced5e00c30e4adf8f33aca
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-27 09:11:13 -0700

    testing printing all numbers 0 - 14 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - ?
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int n;
+	int x;
+	
+	for (n = 0; n <= 9; n++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			if (x >= 10)
+			{
+				_putchar((x / 10) + '0');
+			}
+
+		_putchar((x % 10) + '0');
+
+		}
+	_putchar('\n');
+	}
+}
