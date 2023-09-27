2b08d85c8d65153c864e95985a40396970508e8b
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-27 11:08:25 -0700

    test

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - printing a square based on input
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int x;
+	int s;
+
+	for (x = 0; x < size; x++)
+	{
+		for (s = 0; s < size; s++)
+		{
+			_putchar('#');
+		}
+	}
+}
