9857deca74785c3a2a01ccf8b8ec85755f6027dc
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-27 11:45:15 -0700

    test

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,20 +14,19 @@ int main(void)
 	{
 		if ((x % 3) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if ((x % 5) == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else if ((x % 3) == 0 && (x % 5) == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else 
 		{
-			printf("%d", x);
+			printf("%d ", x);
 		}
 	}
-	return (0);
 }
