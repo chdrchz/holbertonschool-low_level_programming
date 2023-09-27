5ff2332060bb425fb167573e83bc6d8e42511c49
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-27 09:47:32 -0700

    testing diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - printing on a diagonal
+ *
+ * Return: ?
+ */
+void print_diagonal(int n)
+{
+	int s;
+
+	for (s = 0; s < n; s++)
+	{
+		_putchar(' ');
+		_putchar(47);
+	}
+}
