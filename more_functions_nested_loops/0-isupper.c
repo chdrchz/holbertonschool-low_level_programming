d787a2f73edc4881cce7353ce6300dad54a02461
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-27 07:54:09 -0700

    test

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * _isupper - reads if the value is an uppercase letter
- *
+ * @c: variable
  * Return: 1 if it is an uppercase letter, 0 if it is not
  */
 int _isupper(int c)
