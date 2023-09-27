dd11bc7841d83de6c4ef5ee26edae35f1f8c7c89
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-27 08:28:51 -0700

    testing printing all numbers from 0 - 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - printing all numbers 0 - 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n >= 48; n >= 57)
+	{
+		_putchar(n + '0');
+		_putchar('\n');
+	}
+}
