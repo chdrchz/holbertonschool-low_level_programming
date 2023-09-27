733dd75b16a60494b67c5940f90fe41062fa9f1d
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-27 11:39:19 -0700

    testing fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - printing fizzbuzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 100; x++)
+	{
+		if ((x % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else if ((x % 3) == 0 && (x % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else 
+		{
+			printf("%d", x);
+		}
+	}
+}
