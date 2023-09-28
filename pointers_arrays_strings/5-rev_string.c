e4dcb1f9d6df095287e459f9f0035d46fb2533e5
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-28 10:31:59 -0700

    test

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -14,9 +14,10 @@ void rev_string(char *s)
         {
         }
         for (j = i - 1; j != 0; j--)
-        {
-		tmp = s[i];
-		s[i] = s[j];
-		s[j--] = tmp;		
+        {		
         }
+
+	tmp = s[i];
+        s[i] = s[j];
+        s[j--] = tmp;
 }
