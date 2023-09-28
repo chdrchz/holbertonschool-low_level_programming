894ff044f7da66f91e7f8102369554dbe89bfc71
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-28 08:18:56 -0700

    testing loop to print a string to stdout

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 /**
  * _puts - prints a string to the standard output
- *
+ * @str: variable string
  * Return: 0
  */
 void _puts(char *str)
 {
+	int i;
+
 	for (i = 0; str[i] != 0; i++ )
 	{
 		_putchar(i);
 	}
 
 	_putchar('\n');
-
-	return (0);
 }
