09a120f3ea92d40a981a809b6f3592a5120ffecc
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-28 19:33:48 -0700

    creating print array function

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,14 @@ void print_array(int *a, int n)
 {
 	int i;
 	
+	if (i < n - 1)
+        {
+                printf(", ");
+        }
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d, ", a[i]);
-	}
-	if (i < n - 1)
-	{
-		printf(", ");
 	}
 	_putchar('\n');
 }
