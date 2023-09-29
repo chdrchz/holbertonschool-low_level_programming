c6a3c36754989bf3290c1f2c294792e2a676829f
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-28 17:34:13 -0700

    prints half a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts_half - prints half of the string
+ * @str: variable is a pointer to a string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i;
+	
+	len = _strlen(str);
+
+	for (i = 0, i < len / 2; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
