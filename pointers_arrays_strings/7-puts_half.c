76ce666320683510cad36dc191066a7b22b76998
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-28 18:09:51 -0700

    test

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,11 @@ void puts_half(char *str)
 {
 	int i;
 	int len;
-	char tmp;
 
 	len = _strlen(str);
 	
-	for (i = 0; i < len / 2; i++);
+	for (i = 0; i < len / 2; i++)
 	{
-		tmp = str[len];
 		_putchar(str[len]);
 	}
 	_putchar('\n');
