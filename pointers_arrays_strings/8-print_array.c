4cf6a1d5c151f5d329da42e609eea3f14dc6d2f2
Savanna Davis <saavannaa.rose@gmail.com>
2023-09-28 20:07:58 -0700

    testing array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,23 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
-	if (i < n - 1)
+
+	if (n > 0)
 	{
-		printf(", ");
+		for (i = 0; i < n; i++)
+		{
+			if (i < (n - 1))
+			{
+				printf("%d, ", a[i]);
+			}
+			else
+			{
+			printf("%d\n", a[i]);
+			}
+		}
 	}
-	
-	for (i = 0; i < n; i++)
+	else
 	{
-		printf("%d, ", a[i]);
+		printf("\n");
 	}
-	_putchar('\n');
 }
