58cc8fdbbfa09b7265f11f1fae19f1735e0f0059
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-04 06:53:11 -0700

    removing whitespace

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,7 @@
 char *_strcat(char *dest, char *src)
 {
 	char *ptr = dest;
-	
+
 	while (*ptr != '\0')
 	{
 		ptr++;
@@ -21,6 +21,6 @@ char *_strcat(char *dest, char *src)
 		src++;
 		*ptr = '\0';
 	}
-	
+
 	return (dest);
 }
