3c5ae46578502f38f0c45ff8d0dd55ced7384b04
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-04 11:44:27 -0700

    testing changing to uppercase character

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *cap_string - capitalizes the first letter of a word in a string
+ * @k: string variable
+ *
+ * Return: k (the capitalized string)
+ */
+char *cap_string(char *k)
+{
+	int i = 0;
+	int j;
+	int cspc = 13;
+	int spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (k[i])	
+	{
+		j = 0;
+
+		while (i < cspc)
+		{
+			if ((i == 0 || k[i] - 1 == spc[j]) && (k[i] >= 97 && k[i] <= 122))
+			{
+				k[i] -= 32;
+			}
+
+			j++;
+		}
+	i++;
+	
+	}
+
+	return (k);
+}
