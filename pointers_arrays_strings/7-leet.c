d8579407b0e4418d77ed351f60e7c00ee0a0a8c2
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-04 12:58:12 -0700

    testing leet

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *leet - encoding a string into leet code
+ *
+ * Return: char
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *s + i; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
+}
