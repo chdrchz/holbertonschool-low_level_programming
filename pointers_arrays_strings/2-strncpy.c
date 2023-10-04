cd2636d24dc6863a4bf2b8fe93f1209f620f5e83
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-04 08:04:10 -0700

    tetsing copying a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int dest_len = _strlen(dest);
 
 	for (i = 0; i < n && src[i]; i++)
 	{
-		dest[dest_len] = src[i];
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		i++;
+		dest[i] = '\0';
 	}
 
 	return (dest);
