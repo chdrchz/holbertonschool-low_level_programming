7c5758f6c549f67803b661c7197c94182d54235d
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-04 10:05:24 -0700

    fixing spacing

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -15,13 +15,13 @@ int _strcmp(char *s1, char *s2)
 		s2++;
 	}
 
-	if(*s1 == *s2)
+	if (*s1 == *s2)
 	{
-		return(0);
+		return (0);
 	}
 	else
 	{
-		return(*s1 - *s2);
+		return (*s1 - *s2);
 	}
 
 }
