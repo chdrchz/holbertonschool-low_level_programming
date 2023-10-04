1dfebb8a3c3a97c9c78ef567f859eee9fba33832
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-04 07:47:26 -0700

    removing whitespace and adding documentation

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -3,19 +3,21 @@
 #include <stdio.h>
 /**
  * *_strncat - concatonate 2 strings at n bytes
- *
+ * @dest: destination
+ * @src: source
+ * @n: bytes
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int dest_len = _strlen(dest);
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[dest_len + i] = src[i]; 
+		dest[dest_len + i] = src[i];
 	}
-	
+
 	dest[dest_len + i] = '\0';
 
 	return (dest);
