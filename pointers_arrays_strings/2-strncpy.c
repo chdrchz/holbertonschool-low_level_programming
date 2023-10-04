43064f93e4b1cd89027bb0c4e16153b24cd0dda0
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-04 07:55:33 -0700

    test

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+#include <stdio.h>
+/**
+ * *_strncpy - copies a string
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int dest_len = _strlen(dest);
+
+	for (i = 0; i < n && src[i]; i++)
+	{
+		*dest = src[i];
+	}
+
+	return (dest);
+}
