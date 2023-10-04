495ea0da84c72a258b7d26e02fbb08d12ccc8a8a
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-04 10:35:16 -0700

    test

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,7 +12,9 @@ void reverse_array(int *a, int n)
 	int j;
 
 	for (i = 0; (i < (n - 1) / 2); i++)
-		j = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = j;
+	{
+	}
+	j = a[i];
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = j;
 }
