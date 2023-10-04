a9d41c5d52b20772876cb18926f66d985d473705
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-04 13:03:16 -0700

    testing leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,21 +6,17 @@
  */
 char *leet(char *s)
 {
-	int i;
-	int j;
+	int i, j;
 	char a[] = "aAeEoOtTlL";
 	char b[] = "4433007711";
 
-	for (i = 0; *s + i; i++)
+	for (i = 0; *(s + i); i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			if (a[j] == s[i])
-			{
 				s[i] = b[j];
-			}
 		}
 	}
-
 	return (s);
 }
