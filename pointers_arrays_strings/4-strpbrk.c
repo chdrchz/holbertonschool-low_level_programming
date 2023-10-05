e938393220ef073cefb52bff251f9bfb0f2a3543
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 09:38:03 -0700

    testing string prefix checkinga nd assigning to a pointer

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strpbrk - locates first occur of char from bytes in accept
+ * @s: string
+ * @accept: bytes 
+ * Return: pointer to the byte in s if matches accept, or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i; 
+	int j;
+
+	for (i = 0; s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+	}
+	return *(i);
+}
