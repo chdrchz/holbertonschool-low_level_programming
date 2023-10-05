a6735748dd8e6965b60fbe98ceffcc6a2d19fb20
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 10:22:56 -0700

    fixing errors

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,16 +9,21 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*haystack != '\0')
 	{
+		start = haystack;
+
 		while (*haystack != '\0' && *needle != '\0' && *haystack == *needle)
 		{
 			haystack++;
 			needle++;
 		}
-		if (!*needle)
+		if (*needle == '\0')
 		{
-			return (*haystack);
+			return start;
 		}
-		haystack = haystack + 1;
+
+		haystack = start + 1;
+		needle = needle - (haystack - start);
 	}
-	return (0);
+
+	return (NULL);
 }
