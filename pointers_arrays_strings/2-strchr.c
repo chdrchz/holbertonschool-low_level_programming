8ac186398994b67171c23e0fdaf99a0df83d4fe0
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 08:51:18 -0700

    testing finding a char in a string and returning the value to a pointer

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (s)
-	{
-		if (*s == c)
-		{
-			break;
-		}
+	int i;
 
-		*s++;
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
 	}
-	return (s);
+
+	return ('\0');
 }
