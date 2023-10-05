5fe4588ef150b3ce30efc389d1d731766c61f89b
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 10:01:27 -0700

    testing comparing strings

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strstr - locates a substring
+ *
+ * Return: pointer to the beginning of sub, or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i; 
+	int j;
+
+	for (i = 0, haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				return (haystack + i);
+			}
+		}
+		return ('\0');
+	}
+}
