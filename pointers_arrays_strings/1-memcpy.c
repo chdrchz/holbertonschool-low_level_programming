ce6452f162a6080cf7ae95683fc16e8c18a9ca7c
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 07:58:35 -0700

    testing memcpy function

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memcpy - copies bytes from memory area to dest
+ *
+ * Return: dest with copied bytes
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		src[i] = dest;
+	}
+
+	return (dest);
+}
