cb2b357ddd10ef41012cac3c329a1343d428ee08
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 09:03:46 -0700

    testing string prefix function

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - matches string prefix
+ * @s: string
+ * @accept: number of bytes
+ *
+ * Return: returns number of initial bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+	}
+
+	return (accept);
+}
