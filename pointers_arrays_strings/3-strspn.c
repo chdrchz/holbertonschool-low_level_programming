ef6e89e60e7d239fb6ffe6dd6b74ff261ae03419
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 09:13:16 -0700

    testing string prefix function

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,11 +10,19 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i;
+	int j;
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
-		s[i] = accept[i];
+		for (j = 0; accept[j] >= '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+			
 	}
 
-	return (*accept);
+	return (i);
 }
