81e0527dbdcb13e0fcd15bd4f3aa2bab3d92fbc5
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 09:38:57 -0700

    it would help if I added counters

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,9 +10,9 @@ char *_strpbrk(char *s, char *accept)
 	int i; 
 	int j;
 
-	for (i = 0; s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
