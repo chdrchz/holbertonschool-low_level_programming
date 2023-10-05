4732fcc6eb462bcf44cb4d8101124dca6741cc8e
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 08:13:19 -0700

    testing memcpy function

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -6,11 +6,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	char tmp;
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		*dest = src[i];
+		tmp = src[i];
+	       	src[i] = dest;
 	}
 
 	return (dest);
