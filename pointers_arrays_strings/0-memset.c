6f59b1251099a40bddc97417d5acaf5ed959d875
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 07:35:34 -0700

    testing memset function

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -8,17 +8,15 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
+	char mem[n];
 
 	if (s == NULL || n == 0)
 	{
 		return NULL;
 	}
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < mem[n]; i++)
 	{
-		s[n] = s[i];
-		s[i] = s[b + '0'];
 		i++;
 	}
-	return s;
 }
