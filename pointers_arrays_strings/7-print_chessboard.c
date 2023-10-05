d70afd4704d0e2e3e61c8209bd691fd09dfb0ea6
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 12:36:33 -0700

    testing printing chess board

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - prints chessboard
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	char board[] = {"'r'", "'k'", "'b'", "'q'", "'k'", "'b'", "'k'", "'r'",
+			"'p'", "'p'", "'p'", "'p'", "'p'", "'p'", "'p'", "'p'",
+			"' '", "' '", "' '", "' '", "' '", "' '", "' '", "' '",
+			"' '", "' '", "' '", "' '", "' '", "' '", "' '", "' '",
+			"' '", "' '", "' '", "' '", "' '", "' '", "' '", "' '",
+			"' '", "' '", "' '", "' '", "' '", "' '", "' '", "' '",
+			"'p'", "'p'", "'p'", "'p'", "'p'", "'p'", "'p'", "'p'",
+			"'r'", "'k'", "'b'", "'q'", "'k'", "'b'", "'k'", "'r'"};
+	
+	int i;
+	char position;
+	
+	position = sizeof(board)/sizeof(board[0]);
+
+	for (i = 0; i < position; i++)
+	{
+		_putchar(board[position]);
+	}
+
+	return (0);
+}
