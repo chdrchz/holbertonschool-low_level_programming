f33303e50023da53f267560848dbd14a31b11ca5
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 08:18:16 -0700

    fixing documentation

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
  * *_memcpy - copies bytes from memory area to dest
+ * @dest: destination to copy memory to 
+ * @src: source of memory to copy
+ * @n: number of bytes
  *
  * Return: dest with copied bytes
  */
