eba223cdf557caca6bc580a4dfe88309da9aa0c3
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-05 09:44:18 -0700

    removing whitespace

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,12 +2,12 @@
 /**
  * *_strpbrk - locates first occur of char from bytes in accept
  * @s: string
- * @accept: bytes 
+ * @accept: bytes
  * Return: pointer to the byte in s if matches accept, or NULL
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i; 
+	int i;
 	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
