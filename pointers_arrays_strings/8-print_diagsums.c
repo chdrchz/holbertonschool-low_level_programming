af3a53a07fa5d379e8afb27f8ece20abde0a99e9
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-06 08:08:05 -0700

    testing printing the sums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -8,21 +8,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int sum1 = 0;
-	int sum2 = 0;
+    int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < size * size; i++)
-	{
-		if (i % (size + 1) == 0)
-		{
-			sum1 += a[i];
-		}
-		if (i % (size - 1) == 0 && i != 0 && i != (size - 1) * size)
-		{
-			sum2 += a[i];
-		}
-	}
-	_putchar(sum1);
-	_putchar(sum2);
-}
+    for (i = 0; i < size * size; i++)
+    {
+        if (i % (size + 1) == 0)
+        {
+            sum1 += a[i];
+        }
+
+        if (i % (size - 1) == 0 && i != 0 && i != (size - 1) * size)
+        {
+            sum2 += a[i];
+        }
+    }
