65f92e4a34934bde4bd6101b27ee7a96de461044
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-06 09:12:43 -0700

    adding a new line

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,8 +17,8 @@ void print_diagsums(int *a, int size) {
 		sum1 += a[i * size + i];
 		sum2 += a[i * size + (size - 1 - i)];
 	}
-
+	
+	_putchar('\n');
 	printf("%d, ", sum1);
 	printf("%d", sum2);
-	_putchar('\n');
 }
