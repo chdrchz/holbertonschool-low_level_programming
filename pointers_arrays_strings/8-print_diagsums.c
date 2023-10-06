5a8a358bbdce3a0e7e9c0598202fd1df535f719c
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-06 07:48:37 -0700

    printing sum of diagonal in a matrix

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_diagsums - printing the sum of the square matrix
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	unsigned int i, j;
+
+	i = 0;
+	
+	while (i < size)
+	{
+		j = 0;
+
+		while (j < size)
+		{
+			printf("%d", a[i][j]);
+			j++;
+		}
+
+		_putchar('\n')
+		i++;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		sum = sum + a[i][i];
+		size = size + a[i][size - i - 1]
+	}
+}
