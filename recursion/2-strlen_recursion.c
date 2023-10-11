f8af5e5ae0a52a46785d9114a0c3813bd7802a7c
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-11 07:49:57 -0700

    removing whitespace and fixing documentation

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,11 +3,17 @@
 #include "main.h"
 /**
  * int _strlen_recursion - returns the length of a string
- *
- * Return: int
+ * @s: pointer to a string
+ * Return: length of string in int
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0') return 0;
-	else return 1 + _strlen_recursion(s + 1);
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 + _strlen_recursion(s + 1);
+	}
 }
