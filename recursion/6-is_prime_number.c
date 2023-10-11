1f1ca8f474ad2a704cb344c990f167c7f1cca724
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-11 11:35:16 -0700

    testing function to print prime numbers

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * is_prime_number - tests an inputted number and checks if it is prime
+ *
+ * Return: 1 if prime, 0 if not
+ */
+int is_prime_number(int n)
+{
+	int divider = 2;
+
+	if (n % divider == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+	return(is_prime_number(n));
+}
