6dfa0b30a58c9bdbcc3597976ae30b776838197e
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-11 08:47:32 -0700

    testing printing square root recursively

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _sqrt_recursion - calculates the square root recursively
+ * @n: input int
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	est = n;
+	new_est = 0.5 * (est + n / est);
+
+	return (_sqrt_recursion(n));
+}
