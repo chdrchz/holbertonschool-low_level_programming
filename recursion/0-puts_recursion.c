c812e1b8aacf0cf01ee70f72557c376a22b9962a
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-11 07:16:30 -0700

    adding documentation

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _puts_recursion - stores memory from a variable
+ * @s: pointer to a char
+ *
+ * Return: pointer to a string
+ */
 void _puts_recursion(char *s)
 {
 	puts(s);
