62b458913b25ef01ca11c7822a43458d8fdfecb5
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-11 19:26:39 -0700

    function to compare if two strings are identical

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -8,19 +8,17 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	int len1;
-	int len2;
-
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
-	if (len1 == len2)
+	if (s1* == *s2)
 	{
 		return (1);
 	}
-	else
+	if (*s1 == '\0' && *s2 == '\0')
 	{
-		return (0);
+		return (1);
+	}
+	if (*s2 == '*')
+	{
+		return (s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2));
 	}
 	return (wildcmp(s1 + 1, s2 + 1));
 }
