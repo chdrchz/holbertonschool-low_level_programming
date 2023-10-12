77173a05b6a32b85c14f7804ebb7b7f291ffb2e5
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-11 19:29:00 -0700

    fixing pointer

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
-		return (s1 + 0, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2));
+		return (s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2));
 	}
 	return (wildcmp(s1 + 1, s2 + 1));
 }
