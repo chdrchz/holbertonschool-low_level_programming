e1a38082ef00d41d6a19deae2408c116a226368b
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-13 10:49:47 -0700

    argc is unused

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 /**
  * main - multiplies two numbers
- * @argc: number of arguments
+ * @argc: number of arguments (unused)
  * @argv: array of arguments (string)
  * Return: void
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	int number_One = atoi(argv[1]);
 	int number_Two = atoi(argv[2]);
