6962b7ce85c8fddffd96b45b13570cea79fc0c21
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-13 10:59:53 -0700

    fixing if

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,17 +8,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int number_One = atoi(argv[1]);
-        int number_Two = atoi(argv[2]);
-
-	int result_Of_Mult = number_One * number_Two;
-	
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	
+	int number_One = atoi(argv[1]);
+        int number_Two = atoi(argv[2]);
+
+        int result_Of_Mult = number_One * number_Two;
+
 	printf("%d\n", result_Of_Mult);
 	
 	return (0);
