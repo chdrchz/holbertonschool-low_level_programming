ba5cb295391eac1f3148cbcf1cc2240bc9ef8d3c
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-13 11:09:25 -0700

    removing whitespace and spaces before tabs

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,19 +9,19 @@
 int main(int argc, char *argv[])
 {
 	int number_One, number_Two, result_Of_Mult;
-	
+
 	if (argc != 3)
-        {
-        	printf("Error\n");
-        	return (1);
-        }
+	{
+		printf("Error\n");
+		return (1);
+	}
 
 	number_One = atoi(argv[1]);
-        number_Two = atoi(argv[2]);
+	number_Two = atoi(argv[2]);
 
-        result_Of_Mult = number_One * number_Two;
-	
+	result_Of_Mult = number_One * number_Two;
+
 	printf("%d\n", result_Of_Mult);
-	
+
 	return (0);
 }
