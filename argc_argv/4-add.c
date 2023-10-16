3d061d6e0c50a859674b66e7284c38b73e53b781
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-16 13:33:56 -0700

    testing different function instead of atoi

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,35 +8,42 @@
  */
 int main(int argc, char *argv[])
 {
-int counter;
-long number;
-int sum_Of_Numbers = 0;
+    int counter;
+    long number; 
 
-if (argc < 2)
-{
-printf("0\n");
-return 0;
+    if (argc < 2)
+    {
+        printf("0\n");
+        return 0;
+    }
+
+    int sum_Of_Numbers = 0;
+
+    for (counter = 1; counter < argc; counter++)
+    {
+        char *endptr = argv[counter];
+
+        while (*endptr != '\0')
+        {
+            if (!isdigit(*endptr) && !(endptr == argv[counter]))
+            {
+                printf("Error: Not a valid integer\n");
+                return 1;
+            }
+            endptr++;
+        }
+
+        number = strtol(argv[counter], NULL, 10);
+
+        if (number < 0 || (number < 48 && number > 57))
+        {
+            printf("Error: Number out of range\n");
+            return 1;
+        }
+
+        sum_Of_Numbers += number;
+    }
+    printf("%ld\n", sum_Of_Numbers);
+
+    return 0;
 }
-
-for (counter = 1; counter < argc; counter++)
-{
-char *endptr;
-number = strtol(argv[counter], &endptr, 10);
-
-if (*endptr != '\0')
-{
-return 1;
-}
-
-if (number < 0 || (number < 48 && number > 57))
-{
-return 1;
-}
-
-sum_Of_Numbers += number;
-}
-printf("%d\n", sum_Of_Numbers);
-
-return 0;
-}
-
