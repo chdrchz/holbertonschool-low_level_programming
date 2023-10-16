1b3bf786d637841d8255210bca93a18026f56736
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-16 13:44:36 -0700

    removing spacing

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,40 +9,41 @@
  */
 int main(int argc, char *argv[])
 {
-    int counter;
-    long number; 
-    long sum_Of_Numbers = 0;
+	int counter;
+	long number; 
+	long sum_Of_Numbers = 0;
 
-    if (argc < 2)
-    {
-        printf("0\n");
-        return 0;
-    }
+	if (argc < 2)
+	{
+		printf("0\n");
+		return 0;
+	}
 
-    for (counter = 1; counter < argc; counter++)
-    {
-        char *endptr = argv[counter];
+	for (counter = 1; counter < argc; counter++)
+	{
+		char *endptr = argv[counter];
 
-        while (*endptr != '\0')
-        {
-            if (!isdigit(*endptr) && !(endptr == argv[counter]))
-            {
-                printf("Error\n");
-                return 1;
-            }
-            endptr++;
-        }
+		while (*endptr != '\0')
+		{
+			if (!isdigit(*endptr) && !(endptr == argv[counter]))
+			{
+			printf("Error\n");
+			return 1;
+			}
+		endptr++;
+		}
 
-        number = strtol(argv[counter], NULL, 10);
+	number = strtol(argv[counter], NULL, 10);
 
-        if (number < 0 || (number < 48 && number > 57))
-        {
-            return 1;
-        }
+	if (number < 0 || (number < 48 && number > 57))
+	{
+	return 1;
+	}
 
-        sum_Of_Numbers += number;
-    }
-    printf("%ld\n", sum_Of_Numbers);
+	sum_Of_Numbers += number;
+	}
 
-    return 0;
+	printf("%ld\n", sum_Of_Numbers);
+
+	return 0;
 }
