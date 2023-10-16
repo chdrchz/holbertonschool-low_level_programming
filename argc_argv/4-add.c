85dbb3b9aa849a59fb222dcf3cf7a4049ea41b58
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-16 13:25:11 -0700

    testing different function instead of atoi

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,31 +8,36 @@
  */
 int main(int argc, char *argv[])
 {
-	int counter, number;
-	int sum_Of_Numbers = 0;
+int counter;
+long number;
 
-	if (argc < 2)
-	{
-		printf("0");
-	}
-
-	for (counter = 1; counter < argc; counter++)
-	{
-		number = atoi(argv[counter]);
-		
-		if (number < 0)
-		{
-			return (1);
-		}
-
-		else if (number < 48 && number > 57)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum_Of_Numbers += number;
-	}
-	printf("%d\n", sum_Of_Numbers);
-
-	return (0);
+if (argc < 2)
+{
+printf("0\n");
+return 0;
 }
+
+int sum_Of_Numbers = 0;
+
+for (counter = 1; counter < argc; counter++)
+{
+char *endptr;
+number = strtol(argv[counter], &endptr, 10);
+
+if (*endptr != '\0')
+{
+return 1;
+}
+
+if (number < 0 || (number < 48 && number > 57))
+{
+return 1;
+}
+
+sum_Of_Numbers += number;
+}
+printf("%d\n", sum_Of_Numbers);
+
+return 0;
+}
+
