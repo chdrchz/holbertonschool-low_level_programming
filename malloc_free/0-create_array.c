106862cb18bd118fdbe6ca194ba402d17b79a22a
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-18 07:49:26 -0700

    fixing i declaration

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,13 +8,14 @@
 char *create_array(unsigned int size, char c)
 {
 	char *char_array = (char*) malloc(sizeof(char) * size);
+	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
 	
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		char_array[i] = c;
 	}
