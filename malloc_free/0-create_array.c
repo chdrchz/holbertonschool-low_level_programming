aac1bc9e34a457a049757b09a6bacc882387f8cb
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-18 07:59:55 -0700

    testing variable declarations

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -7,13 +7,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *char_array = (char*) malloc(sizeof(char) * size);
+	unsigned int i;
+	
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	
-	char *char_array = (char*) malloc(sizeof(char) * size);
-	unsigned int i;
 
 	if (char_array == NULL)
 	{
