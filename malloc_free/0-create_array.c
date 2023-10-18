eecc417f9ff8581835e49f512ac8691e96ea6325
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-18 07:52:59 -0700

    handling return of malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 	char *char_array = (char*) malloc(sizeof(char) * size);
 	unsigned int i;
 
-	if (size == 0)
+	if (size == NULL)
 	{
 		return (NULL);
 	}
