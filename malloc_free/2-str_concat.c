8d852f2abad1b761036d92f1f3bac073a4dbb4a9
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-18 09:53:39 -0700

    adding null terminator

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,40 +8,39 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int j;
-	char *char_array;
-	int length_1 = _strlen(s1);
-	int length_2 = _strlen(s2);
+    if (!s1)
+    {
+        s1 = "";
+    }
+    if (!s2)
+    {
+        s2 = "";
+    }
 
-	if (!s1)
-	{
-		s1 = "";
-	}
-	if (!s2)
-	{
-		s2 = "";
-	}
-	char_array = (char *)malloc(length_1 + length_2 + 1);
+    size_t length_1 = strlen(s1);
+    size_t length_2 = strlen(s2);
+    char *char_array = (char *)malloc(length_1 + length_2 + 1);
 
-	if (char_array)
-	{
-	
-		for (i = 0; i <= length_1; i++)
-		{
-			char_array[i] = s1[i];
-		}
-		for (j = 0; j <= length_2; j++)
-		{
-			char_array[i] = s2[j];
-			i++;
-		}
-		
-		char_array[i] = '\0';
-		return (char_array);
-	}
-	else
-	{
-		return (0);
-	}
+    if (char_array)
+    {
+        size_t i, j;
+
+        for (i = 0; i < length_1; i++)
+        {
+            char_array[i] = s1[i];
+        }
+
+        for (j = 0; j < length_2; j++)
+        {
+            char_array[i] = s2[j];
+            i++;
+        }
+
+        char_array[i] = '\0';
+        return char_array;
+    }
+    else
+    {
+        return NULL;
+    }
 }
