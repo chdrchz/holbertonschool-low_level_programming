363bfcba8797bbff12b951dc9b04e28a42735a83
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-18 11:11:22 -0700

    pleasing betty

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -8,33 +8,23 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int w_counter;
-	int h_counter;
-	int i;
-	int **grid;
+	int w_counter, h_counter, i, **grid;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-
 	grid = (int **)malloc(sizeof(int *) * height);
-
 	if (grid)
 	{
 		for (h_counter = 0; h_counter < height; h_counter++)
 		{
 			grid[h_counter] = (int *)malloc(sizeof(int) * width);
-
 			if (!grid[h_counter])
-			{
 				for (i = 0; i < h_counter; i++)
-				{
 					free(grid[i]);
-				}
 				free(grid);
 				return (NULL);
-			}
 		}
 		for (h_counter = 0; h_counter < height; h_counter++)
 		{
@@ -46,7 +36,5 @@ int **alloc_grid(int width, int height)
 		return (grid);
 	}
 	else
-	{
 		return (NULL);
-	}
 }
