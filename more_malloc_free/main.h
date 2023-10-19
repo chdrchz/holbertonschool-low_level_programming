3facbf85e6a757ecd1e023fe5938be058bf6a295
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-19 09:29:23 -0700

    added _strlen.c

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN
 #define MAIN
 
+int _strlen(char *s);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
