39b9dd99a246a9c7e3f21cdd56c52005615fa9f5
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-20 07:13:33 -0700

    program to print the name form the file it was compiled

---
+++ preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include "stdlib.h"
+#include <stdio.h>
+/**
+ * main - printing the name of the file from which it was compiled
+ * @argc: name of arguments
+ * @argv: number of arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	if argc > 0;
+	{	
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
