2213519529377026b04e26e4f14980269e29fa5b
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-25 10:36:37 -0700

    handling malloc return

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,26 +10,32 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_doggy;
-	if (name == NULL || age < 0 || owner == NULL)
+	if (new_doggy)
 	{
-		return (NULL);
-	}
-	new_doggy = malloc(sizeof(dog_t));
-	new_doggy->name = (char *)malloc(strlen(name) + 1);
-	if (new_doggy->name == NULL)
+		if (name == NULL || age < 0 || owner == NULL)
+		{
+			return (NULL);
+		}
+		new_doggy = malloc(sizeof(dog_t));
+		new_doggy->name = (char *)malloc(strlen(name) + 1);
+		if (new_doggy->name == NULL)
+		{
+			free (new_doggy);
+			return (NULL);
+		}
+		new_doggy->owner = (char *)malloc(strlen(owner) + 1);
+		if (new_doggy->owner == NULL)
+		{
+			free (new_doggy->name);
+			free (new_doggy);
+			return (NULL);
+		}
+		new_doggy->name = strcpy(new_doggy->name, name);
+		new_doggy->owner = strcpy(new_doggy->owner, owner);
+		new_doggy->age = age;
+		return (new_doggy);
+	else
 	{
-		free (new_doggy);
-		return (NULL);
+		return (new_doggy);
 	}
-	new_doggy->owner = (char *)malloc(strlen(owner) + 1);
-	if (new_doggy->owner == NULL)
-	{
-		free (new_doggy->name);
-		free (new_doggy);
-		return (NULL);
-	}
-	new_doggy->name = strcpy(new_doggy->name, name);
-	new_doggy->owner = strcpy(new_doggy->owner, owner);
-	new_doggy->age = age;
-	return (new_doggy);
 }
