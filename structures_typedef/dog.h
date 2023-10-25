6d6a61547480ad5f65d125e99e2679361f5f14eb
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-25 09:21:25 -0700

    test

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,9 +16,15 @@ struct dog
 	char *owner;
 };
 
+#endif
+
+#ifndef MAIN_PROTO
+#define MAIN_PROTO
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
+
