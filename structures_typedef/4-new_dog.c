4a3b5ddb8dc0c8374d7f4697643c9f448657b36a
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-25 10:29:12 -0700

    testing allocating memory to new elements and copying two

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * *new_dog - creates a new dog
+ *
+ * Return: dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_doggy;
+	if (name == NULL || age < 0 || owner == NULL);
+		return (NULL);
+	new_doggy = malloc(sizeof(dog_t));
+	new_doggy->name = (char *)malloc(strlen(name) + 1);
+	if (new_doggy->name == NULL)
+		free (new_doggy);
+		return (NULL);
+	new_doggy->owner = (char *)malloc(strlen(owner) + 1);
+	if (new_doggy-> == NULL)
+	{
+		free (new_doggy->name)
+		free (new_doggy);
+		return (NULL);
+	}
+	new_doggy->name = strcpy(new_doggy->name, name);
+	new_doggy->owner = strcpy(new_doggy->owner, owner);
+	new_doggy->age = age;
+	return (new_doggy);
+}
