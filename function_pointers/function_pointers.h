30ff6ff463b7c783bb0d66429538b1a78443b33e
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-26 11:57:08 -0700

    test

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,10 +3,8 @@
 #ifndef MAIN
 #define MAIN
 
-int _putchar(char c);
-void (*f)(char *);
-int _strlen(char *s);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
