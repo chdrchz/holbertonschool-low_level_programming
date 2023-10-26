552a6f667b896924ebb18bfa7bccbaeeae6627ef
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-25 18:19:42 -0700

    readme, .h and first c file testing compile

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include ""
+/**
+ * print_name - ?
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(char);
+}
+
+void (*f)(char *)
+{	
+	int counter;
+
+	if (counter = 0; counter < name; counter++)
+	{
+		_putchar(char);
+	}
+}
---
+++ function_pointers/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - puts a character
+ *
+ * Return: int
+ */
+int _putchar(char)
+{
+	return (write(1, &c, 1));
+}
