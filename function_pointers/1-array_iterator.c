368397781e8892f7b46eb2e5ebb820ae30f13b85
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-26 07:19:39 -0700

    pleasing betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,18 +2,19 @@
 #include <stddef.h>
 /**
  * array_iterator - iterates through an array
- *
+ * @array: array to be iterated through 
+ * @size: size of array 
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	size_t counter;
 
 	if (array != NULL && action != NULL)
 	{
-		for (i = 0; i < size; i++)
+		for (counter = 0; counter < size; counter++)
 		{
-			action(array[i]);
+			action(array[counter]);
 		}
 	}
 }
