ca5277cdb92b485c8d8fb7f15192d45d0e01af1c
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-28 11:34:41 -0700

    test

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,33 +10,26 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int num1, num2, result;
+	int (*fun)(int, int);
+
+	if (argc != 4)
 	{
-		printf("Error\n");
+		printf("Error\n")
 		return (98);
 	}
 
-	int num1, num2, result;
-	char operand;
-	result = 0;
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	operand = argv[0][2];
+	fun = get_op_func(argv[2]);
 
-	if (strlen(argv[2]) != 1)
+	if (fun == NULL)
 	{
-		printf("Error\n");
+		printf("Error\n")
 		return (99);
 	}
-	if (operand != '+') &&
-		(operand != '-') &&
-		(operand != '*') &&
-		(operand != '/') &&
-		(operand != '%');
-	{
-		printf("Error\n");
-		return (100);
-	}
-	printf("%d\n", get_op_func(operand)(num1, num2));
+
+	result = fun(num1, num2);
+	printf("%d\n", result);
 	return (0);
 }
