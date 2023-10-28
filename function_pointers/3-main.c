a7f429aff2e97d102ca372b05f46ea5dc9258d60
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-28 10:48:38 -0700

    test of calculator

---
+++ function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - performs operations
+ * @argc: argument count
+ * @argv: arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, operand;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operand = atoi(argv[2]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if (operand == '/' && num2 == 0) ||
+		(operand == '%' && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - adds two ints
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two ints
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two ints
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two ints
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulos two ints
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
