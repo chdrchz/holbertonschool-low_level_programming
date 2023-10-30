98a55c88faa157b52b53208d990eeac94039210f
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-29 19:46:34 -0700

    pleasing betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,8 +10,9 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	unsigned int k = 0;
-	
+
 	va_list args;
+
 	va_start(args, n);
 
 	if (n == 0)
@@ -22,6 +23,7 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		int j = va_arg(args, int);
+
 		k += j;
 	}
 
