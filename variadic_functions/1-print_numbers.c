e9dd53ad301bec0b22c993fe998e859e59036994
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-29 20:31:20 -0700

    removing whitespace

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int j;
 
 	va_list args;
-	
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
@@ -23,11 +23,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", separator);
 		}
-		
+
 		j = va_arg(args, int);
 		printf("%d", j);
 	}
-	
+
 	va_end(args);
 	printf("\n");
 }
