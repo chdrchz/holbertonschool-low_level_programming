6b1e736fccfcc939154d6a51d0b172349791ff7b
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-30 07:24:10 -0700

    testing printing strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: variable for va_arg function
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	const char *str;
+	
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL && i > 0) 
+		{
+			printf("%s", separator);
+		}
+		str = va_arg(arg, int);
+		printf("%s", str);
+	}
+	
+	va_end(args);
+	printf("\n");
+}
