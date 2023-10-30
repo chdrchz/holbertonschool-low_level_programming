0001c96f2f637faeba4a7d5dd40d57938765b89b
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-30 07:26:07 -0700

    fixing variable spelling

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", separator);
 		}
-		str = va_arg(args, int);
+		str = va_arg(args, const char);
 		printf("%s", str);
 	}
 	
