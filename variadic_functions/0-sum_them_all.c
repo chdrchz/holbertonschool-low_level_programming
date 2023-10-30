049c79f3aee3fa1158c902b4f3812aeeaa1b358a
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-29 19:42:18 -0700

    adding if n is 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,13 +11,13 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	unsigned int k = 0;
 	
+	va_list args;
+	va_start(args, n);
+
 	if (n == 0)
 	{
 		return (0);
 	}
-	
-	va_list args;
-	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
