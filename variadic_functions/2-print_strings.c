4810b101631f3f855b77b64d0c097d8112ab5886
Savanna Davis <saavannaa.rose@gmail.com>
2023-10-30 07:32:06 -0700

    testing if for nil

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,15 +17,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
+	if (!str)
+	{
+		printf("(nil)");
+	}
+
 	for (i = 0; i < n; i++)
 	{
 		if (separator != NULL && i > 0) 
 		{
 			printf("%s", separator);
-		}
-		if (str == NULL)
-		{
-			printf("(nil)");
 		}
 		str = va_arg(args, const char*);
 		printf("%s", str);
