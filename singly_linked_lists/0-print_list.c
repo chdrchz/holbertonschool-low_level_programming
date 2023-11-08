7afb2f56507266e9787afc4ff72a6bf099290798
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 08:18:24 -0800

    adding line after declarations and describing h

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 /**
  * print_list - prints a singly linked list
- * @h:
+ * @h: head node
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
