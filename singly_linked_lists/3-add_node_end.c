b126d217671b970f26019d7c98cf9c35f3839a94
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 10:15:52 -0800

    adding a last and new node

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,6 +10,7 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *last_node;
 
 	new_node = malloc(sizeof(list_t));
 
@@ -27,6 +28,13 @@ list_t *add_node_end(list_t **head, const char *str)
 		{
 			*head = new_node;
 		}
+		else 
+		{
+			last_node = *head;
+
+			while (last_node = last_node->next)
+		}
+		last_node->next = new_node;
 	}
 	return (new_node);
 }
