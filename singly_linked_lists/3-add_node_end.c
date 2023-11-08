35fc5ed79656666e329fc12702c1626f5373f3bc
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 10:54:30 -0800

    malloc all nodes

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,6 +12,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *current;
 
+	new_node = malloc(sizeof(list_t));
 	current = malloc(sizeof(list_t));
 
 	if (current == NULL)
