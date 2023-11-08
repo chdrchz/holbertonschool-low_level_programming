17bfcacd9151eabea91ebeab0ef3a0b09ff5b622
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 11:15:42 -0800

    testing new_node instead of current in first if

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -15,7 +15,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	current = malloc(sizeof(list_t));
 
-	if (current == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
@@ -37,6 +37,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		}
 		current->next = new_node;
 	}
-	free (current);
 	return (new_node);
 }
