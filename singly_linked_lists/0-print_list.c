b5ba126ac113477f7bca89996fba4dc2e84f30e7
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 08:14:44 -0800

    adding documentation

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,25 +2,24 @@
 #include <stdio.h>
 /**
  * print_list - prints a singly linked list
- * @h: 
+ * @h:
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
-		else 
+		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		count++;
 	}
-	return(count);
+	return (count);
 }
