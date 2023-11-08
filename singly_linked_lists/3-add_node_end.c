7939080f2600337083d8aff464acca4875e4316b
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 10:10:39 -0800

    testing some rearranging of node declaration

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -21,12 +21,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		}
 		new_node->str = strdup(str);
 		new_node->len = strlen(str);
-		new_node->next = *head;
-		*head = new_node;
+		new_node->next = NULL;
 
-		if (new_node->next == NULL)
+		if (*head == NULL)
 		{
-			new_node->next = NULL;
+			*head = new_node;
 		}
 	}
 	return (new_node);
