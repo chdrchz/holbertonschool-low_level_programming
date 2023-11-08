8db5f4efa115316910eca6112ebf965ef2ea6413
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 08:47:56 -0800

    testing adding an additional node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * *add_node - adds a node to the beginning of list
+ * @head: head node
+ * @str: data in list
+ * Return: On success: address of new node
+ * On failure: NULL 
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t new_node = NULL;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->len = strdup(str);
+	new_node->next = *head;
+	*head = new_node;
+}
