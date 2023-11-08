ca4bfe6a28ce80db465df8ae50f14acfa718a9e2
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 10:52:47 -0800

    testing changes to variable documentation and loop

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,31 +10,31 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
-	list_t *last_node;
+	list_t *current;
 
-	new_node = malloc(sizeof(list_t));
+	current = malloc(sizeof(list_t));
 
-	if (new_node)
+	if (current == NULL)
 	{
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		new_node->str = strdup(str);
-		new_node->len = strlen(str);
-		new_node->next = NULL;
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-		if (*head == NULL)
-		{
-			*head = new_node;
-		}
-		else 
-		{
-			last_node = *head;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else 
+	{
+		current = *head;
 
-			while ((last_node = last_node->next))
+		while ((current->next != NULL))
+		{
+			current = current->next;
 		}
-		last_node->next = new_node;
+		current->next = new_node;
 	}
 	return (new_node);
 }
