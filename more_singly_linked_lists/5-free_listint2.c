df226cea07e8c150da449e873881f24fc25a990e
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 17:47:46 -0800

    freeing a list

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - frees a list
+ * @head: start of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+	
+	while (*head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
