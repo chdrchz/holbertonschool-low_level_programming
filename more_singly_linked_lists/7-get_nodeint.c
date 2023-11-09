2aa58966becf78d3ef18055da22a3f8e5a318115
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-09 14:18:30 -0800

    removing whitespace

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 /**
  * *get_nodeint_at_index - returns the nth node of a list
  * @head: start of list
- * @index: index to return node location 
+ * @index: index to return node location
  * Return: node at index
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
