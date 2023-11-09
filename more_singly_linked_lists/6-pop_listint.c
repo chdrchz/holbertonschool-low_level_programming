0ca174b3d9b901a3d7426440d4549c1b927c1707
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 18:49:28 -0800

    replaced ; with : in argument documentation

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  * pop_listint - deletes head node
- * @head; start of the list
+ * @head: start of the list
  * Return: head node's data
  */
 int pop_listint(listint_t **head)
