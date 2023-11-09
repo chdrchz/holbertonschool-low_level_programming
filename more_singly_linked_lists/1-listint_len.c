146b5d5dd8c06489b0bc9b531433fe563722882b
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 16:28:53 -0800

    returning # of nodes in a list

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - returns number of nodes
+ * @h: head of list 
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
