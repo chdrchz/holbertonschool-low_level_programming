6cca8db9a3131dd5b3435d6f8420fcacafafff00
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-09 14:12:43 -0800

    assigning head to current

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -9,6 +9,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t current;
 	unsigned int count = 0;
 
+	current = head;
+
 	while (current != NULL)
 	{
 		if (count == index)
