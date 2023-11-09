2578c2441103b1027dd5a05ea0be43cc0c2181c2
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-09 14:28:02 -0800

    adding sum of all data

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all data in nodes
+ * @head: start of the list 
+ * Return: sum of node data
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t current;
+	int sum = 0;
+
+	current = head;
+
+	if (*head == NULL)
+		return (0);
+
+	while (current != NULL)
+		sum += current->n;
+
+	return (sum);
+}
