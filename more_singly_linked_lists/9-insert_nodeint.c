f638335a473509ad7283326cf53fc8b7762173eb
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-09 15:09:47 -0800

    changing something to test in loop

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (count == idx - 1)
 		{
-			new_node->next = current->next;
+			new_node = current->next;
 			current->next = new_node;
 			return (new_node);
 		}
