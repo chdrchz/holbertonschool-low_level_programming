6590992233b6abf03e8a27986db13937fba62ca1
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-09 15:06:04 -0800

    replacing index with idx and adding parenthesis around node

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	
 	new_node = malloc(sizeof(listint_t));
 
-	if (new_node = NULL)
+	if ((new_node) = NULL)
 		return (NULL);
 
 	new_node->n = n;
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (current != NULL)
 	{
-		if (count == index - 1)
+		if (count == idx - 1)
 		{
 			new_node->next = current->next;
 			current->next = new_node;
