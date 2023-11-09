b07bdff66aa3565cc0bb3ce287f263bf07ea8146
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-09 14:17:51 -0800

    adding argument declarations

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
  * *get_nodeint_at_index - returns the nth node of a list
- *
+ * @head: start of list
+ * @index: index to return node location 
  * Return: node at index
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
