5bc6026a416e4383832bba452a2399d9cd9d4621
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 18:47:23 -0800

    rearranged logic

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -7,18 +7,18 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
+	int value;
 
 	if (*head == NULL)
 	{
 		return (0);
 	}
+
+	temp = *head;
+	*head = (*head)->next;
+	value = temp->n;
+	free(temp);
 	
-	if (temp != NULL)
-	{
-		*head = (*head)->next;
-		temp = *head;
-		free(temp);
-	}
-	return ((*head)->n);
+	return (value);
 }
