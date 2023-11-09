174cddefefe3604a93f2a72065aaf0bd5c812964
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-08 16:04:07 -0800

    adding stdio.h to file for printf

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_listint - prints data in nodes
- *
+ * @h: head of list
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
