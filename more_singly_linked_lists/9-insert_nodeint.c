467fb0c773608ca19cf754c4801d03b55d14f48a
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-09 15:11:22 -0800

    testing diff change

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
 
 	current = *head;
 
@@ -27,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (count == idx - 1)
 		{
-			new_node = current->next;
+			new_node->next = current->next;
 			current->next = new_node;
 			return (new_node);
 		}
