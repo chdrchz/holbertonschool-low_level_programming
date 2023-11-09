5b58969e6779b0311b42625b20140e2c1c7c20fd
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-09 14:52:34 -0800

    testing adding a new node at index

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * *insert_nodeint_at_index - inserts a new node at a given index
+ * @head: start of list
+ * @idx: index to insert new node
+ * @n: data for the new node
+ * Return: On success: address of new node
+ * On failure: NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t current, new_node;
+	unsigned int count = 0;
+	idx = 0;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		if (count == idx)
+		{
+			current = new_node->n;
+			return (current);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
