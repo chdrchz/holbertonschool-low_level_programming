6a8a40cfa4dc6eb9264f0c2d0d678abf86672111
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-10 09:03:27 -0800

    adding additional logic to determine index in list

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -8,16 +8,34 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp;
+	listint_t *temp, *prev;
 	unsigned int count = 0;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-	if (index == count)
+
+	if (index == 0)
 	{
 		temp = *head;
 		*head = (*head)->next;
-		free (temp);
+		free(temp);
+		return (1);
 	}
+
+	temp = *head;
+
+	while (temp != NULL && count < index)
+	{
+		prev = temp;
+		temp = temp->next;
+		count++;
+	}
+
+	if (temp == NULL)
+		return (-1);
+
+	prev->next = temp->next;
+	free(temp);
+
 	return (1);
 }
