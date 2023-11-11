78753ed8e26014f70074b672101cdfc29d049272
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-11 11:03:22 -0800

    converting binary to unsigned int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: binary string
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int length = 0;
+
+	while (b[length] != '\0')
+	{
+		result <<= 1;
+		if (b[length] == 1)
+		{
+			result |= 1;
+		}
+		length++;
+	}
+	return (result);
+}
