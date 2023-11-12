637a455cfa17b74cb40d81c6b608f1a9e22ad448
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-12 09:14:25 -0800

    fixing argument declaration and removing whitespace

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * get_bit - returns a bit at a given index 
- *
+ * get_bit - returns a bit at a given index
+ * @n: number to be evaluated
+ * @index: index to extract bit from
  * Return: bit at index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int shifted_num;
 	int bit_value;
-	
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
