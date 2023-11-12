bdc3c396709f41dcddb644b719f7c6748fcbd3f1
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-12 10:50:34 -0800

    changing <= to >=

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int mask;
 	int value = 0;
 
-	if (index <= sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	mask = 1UL << index;
