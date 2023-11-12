8e90f4535fb2c891197f332ab2b8f13fa60d55ef
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-12 11:32:31 -0800

    testing flipping bits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * flip_bits - returns the number of bits needed to get from numbers
+ * @n: 
+ * Return: ?
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
