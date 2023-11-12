dd6fcd9d8d81c2bd1d431157264f3f94c516a514
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-12 08:58:03 -0800

    testing new logic

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -4,20 +4,16 @@
  * @n: number to be translated to binary
  * Return: void
  */
-void print_zero()
-{
-	_putchar('0');
-}
-
 void print_binary(unsigned long int n)
 {
-	if (n)
+	if (n == 0)
+		_putchar('0');
+	else 
 	{
-		print_binary(n >> 1);
+		if (n & (n - 1))
+		{
+			print_binary(n >> 1);
+		}
 		_putchar((n & 1) ? '1' : '0');
-	}
-	else if (n == 0)
-	{
-		print_zero();
 	}
 }
