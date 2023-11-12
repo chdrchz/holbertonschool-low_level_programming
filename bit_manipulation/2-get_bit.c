907ab52ca8c5bf60a28bcad6efcc36df89e6c051
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-12 09:12:51 -0800

    testing if logic for max size out of bounds

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int shifted_num;
 	int bit_value;
 	
-	if ((unsigned int)index < 0)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	shifted_num = n >> index;
