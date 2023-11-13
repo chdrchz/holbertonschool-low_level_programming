46bcffd584861ac35fd01a839bae5247ca55f0c7
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 12:44:54 -0800

    fixing % in dprintf, line 43

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 	}
 	if (close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't close to fd &d\n", file_to);
+		dprintf(STDERR_FILENO,"Error: Can't close to fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
