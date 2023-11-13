827962e889d48632bdb3d27d8d99ff2bbe05506d
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-12 19:48:28 -0800

    testing new logic

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -6,9 +6,20 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n)
+	int num_bits, i;
+	unsigned long int bit;
+	
+	if (n == 0)
 	{
-		print_binary(n >> 1);
-		_putchar((n & 1) ? '1' : '0');
+		_putchar('0');
+		return;
 	}
+	num_bits = sizeof(n) * 8;
+
+	bool first = false;
+
+	for (i = num_bits - 1; i >= 0; --i)
+	{
+		bit = (n >> i) & 1;
+	}`
 }
