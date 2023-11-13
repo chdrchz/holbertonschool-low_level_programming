668a2a93dd42dd3872a557309ac6b5c75a1bf917
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 11:02:44 -0800

    removing part of write

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = (text_content, length);
+	wr = (length);
 
 	close(op);
 	return (1);
