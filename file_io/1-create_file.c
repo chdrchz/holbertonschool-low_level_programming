3f617097990e51aad94742d689977c075cd3e7f2
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 11:05:08 -0800

    testing loop logic

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,6 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[length] != '\0')
+			_putchar(text_content[length]);
 			length++;
 	}
 	
