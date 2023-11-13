5577df195c279ec850bcba7a87ca94680b0a557b
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 11:13:01 -0800

    adding write function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = (op, text_content, length);
+	wr = write(op, text_content, length);
 
 	if (op == -1 || wr == -1)
 		return (-1);
