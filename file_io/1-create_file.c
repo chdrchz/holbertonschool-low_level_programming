e63a6f6a6b8ad7ae74c78e6654c82ddf8a9f86b4
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 11:13:54 -0800

    removing whitespace and pleasing betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * create_file - creates a file 
+ * create_file - creates a file
  * @filename: name of the file
  * @text_content: content of the file
  * Return: On success: 1 (the file is created)
@@ -9,8 +9,9 @@
 int create_file(const char *filename, char *text_content)
 {
 	int op, wr, length;
+
 	length = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 
@@ -19,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[length] != '\0')
 			length++;
 	}
-	
+
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, length);
 
