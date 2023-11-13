6a2f17a659f69f1c2e388b0f623dbceb684af9ae
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 11:11:15 -0800

    removing _putchar in loop and accounting for errors

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int op, length;
+	int op, wr, length;
 	length = 0;
 	
 	if (filename == NULL)
@@ -17,13 +17,14 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[length] != '\0')
-		{
-			_putchar(text_content[length]);
 			length++;
-		}
 	}
 	
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wr = (op, text_content, length);
+
+	if (op == -1 || wr == -1)
+		return (-1);
 
 	close(op);
 	return (1);
