899739e4f1caa5f150c2ce0a978586b184c941f0
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 11:03:41 -0800

    removing write completely

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr, length;
+	int op, length;
 	length = 0;
 	
 	if (filename == NULL)
@@ -21,7 +21,6 @@ int create_file(const char *filename, char *text_content)
 	}
 	
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = (length);
 
 	close(op);
 	return (1);
