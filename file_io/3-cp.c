e0d2e1f41886e85a5b50140c46ce25dd980d3785
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 12:36:18 -0800

    testing logic for copying data from one file to another

---
+++ file_io/3-cp.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program to copy content of one file to another
+ * @argc: number of arguments
+ * @argv: list of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to;
+	char buffer[size];
+	ssize_t bytes_read, bytes_written;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while (bytes_read = read(file_from, buffer, size) > 0)
+		bytes_written = write(file_to, buffer, bytes_read);
+	if (file_from == -1 || bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1 || bytes_written != bytes_read)
+	{
+		dprintf(STDERR_FILENO"Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	return (0);
+}
