6404cf46ec3de7f4474f64962a1e318201f91466
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 10:28:25 -0800

    writing text read to stdout

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile - reads from a text file and prints it to the standard output
+ * @filename: name of the file
+ * @letters: objects in file to be read
+ * Return: actual text it can read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, rd, wr;
+	char *buffer;
+	
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	rd = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+
+	free(buffer);
+	close(op);
+
+	return (wr);
+}
