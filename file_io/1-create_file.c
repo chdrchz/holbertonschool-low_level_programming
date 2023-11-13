b6f1797bfc3048b75d5d2ee776e60c44c13b514d
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 11:05:56 -0800

    adding brackets for loop

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,8 +17,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[length] != '\0')
+		{
 			_putchar(text_content[length]);
 			length++;
+		}
 	}
 	
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
