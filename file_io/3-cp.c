9f7fd4a92cbcd63236168d850ef39e6b1c48b5da
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-13 20:06:53 -0800

    adding spaces before statement

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
@@ -25,22 +25,22 @@ int main(int argc, char *argv[])
 		bytes_written = write(file_to, buffer, bytes_read);
 	if (file_from == -1 || bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (file_to == -1 || bytes_written != bytes_read)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	if (close(file_from) == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	if (close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't close to fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close to fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
