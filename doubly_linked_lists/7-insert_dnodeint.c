28e22453f3d870c55be3dfa95897819cc4bf2a7b
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-15 09:45:08 -0800

    fixing variable declarations

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *current;
-	unsigned int count == 0;
+	unsigned int count = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
 
