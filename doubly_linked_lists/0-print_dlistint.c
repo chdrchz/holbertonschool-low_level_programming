9193cd941cbe5499c6bb6ec4e891a857b9a2f64f
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-15 08:06:18 -0800

    putting in a loop

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	if (h->n == 0)
-		return (0);
-	else 
-		printf("%d\n", h->n);
+	while (h != NULL)
+	{
+		if (h->n == 0)
+			return (0);
+		else 
+			printf("%d\n", h->n);
+	}
 	h = h->next;
 	count++;
 
