2393b2a1606f2d06594a384fd60a81f10b995163
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-16 09:03:03 -0800

    removing semi colon

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	prev_node = NULL;
 
-	while (current != NULL && count < idx;)
+	while (current != NULL && count < idx)
 	{
 		prev_node = current;
 		current = current->next;
