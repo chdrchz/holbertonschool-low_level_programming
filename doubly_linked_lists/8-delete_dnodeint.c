c84e85248d76da50c58c29bc67da0e25439d0954
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-16 11:48:05 -0800

    adding logic for multiple nodes

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -33,6 +33,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (temp == NULL)
 		return (-1);
 	prev->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = prev;
 	free(temp);
 	return (1);
 }
