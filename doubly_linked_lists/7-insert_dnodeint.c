e322ee55c13d6a217520247df34c3a6df3a0b3a2
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-16 07:20:17 -0800

    testing link restablishment of prev

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -24,8 +24,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->next = *h;
+		*h->prev = new_node;
 		*h = new_node;
-		new_node->prev = NULL;
 		return (new_node);
 	}
 
