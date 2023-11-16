1facfb0fc25d77d1047510e8ad14bb88440c5971
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-16 08:39:27 -0800

    testing complete logic change

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,38 +10,20 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current;
 	unsigned int count = 0;
+	dlistint_t *prev_node, *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
 	new_node->n = n;
-	current = *h;
 
-	if (idx == 0)
-	{
-		new_node->next = current;
-		current->prev = new_node;
-		current = new_node;
-		return (new_node);
-	}
-
-	while (current != NULL)
-	{
-		if (count == idx - 1)
-		{
-			new_node->next = current->next;
-			current->next = new_node;
-			new_node->prev = current;
-			current = new_node->next;
-			current->prev = new_node;
-			return (new_node);
-		}
+	while (new_node != NULL)
 		count++;
-		current = current->next;
+
+	if (count == index)
+	{
+		new_node->next = prev-node->next;
+		prev_node->next = new_node;
+		new_node->prev = prev_node;
 	}
-	return (NULL);
+	return (new_node);
 }
