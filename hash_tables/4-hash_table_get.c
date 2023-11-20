95e6274290b2eb02846c62d34c1945492216c559
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-20 10:27:32 -0800

    adding null checks

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current;
 	int index;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 
 	current = ht->array[index];
