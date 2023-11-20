25894ea92260a3019790df0c768f78eabbc31af3
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-20 11:21:51 -0800

    adding flag to track pairs

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,6 +9,7 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current, *next;
 	unsigned long int counter;
+	int pair = 1;
 
 	printf("{");
 	
@@ -19,11 +20,15 @@ void hash_table_print(const hash_table_t *ht)
 		while (current != NULL)
 		{
 			next = current->next;
+
+			if (!pair)
+				printf(",");
 			printf("'%s': '%s'", current->key, current->value);
 
 			if (next != NULL)
 				printf(",");
-
+			
+			pair = 0;
 			current = next;
 		}
 	}
