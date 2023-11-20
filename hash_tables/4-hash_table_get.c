3f2698fd2c95fad63d7890478a82d768147f3d08
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-20 10:20:51 -0800

    adding skeleton

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves value associated with a key
+ * @ht: hash table
+ * @key: key to get value from
+ *
+ * Return: On success: value associated with the element
+ * On failure: NULL if key cannot be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	int index;
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current && strcmp(current->key, key) != '\0')
+		current = current->next;
+
+	return (current->value);
+
+}
