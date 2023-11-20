a1e5294bfc5fb75b1095544a09b29a503bf6a5c6
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-20 10:59:37 -0800

    testing simple logic to print hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current;
+	unsigned long int counter;
+	
+	for (counter = 0; counter < ht->size; counter++)
+	{
+		current = ht->array[counter];
+
+		while (current != NULL)
+		{
+			printf("%s, %s", current->key, current->value);
+			current = current->next;
+		}
+	}
+}
