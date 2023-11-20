d02641a27434fce8e59916c9dd17d6e8db603a47
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-20 09:08:12 -0800

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -40,7 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->key = strdup(key);
 	new_node->value = value_copy;
 	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	
+	ht->array[index] = new_node;	
 	return (1);
 }
