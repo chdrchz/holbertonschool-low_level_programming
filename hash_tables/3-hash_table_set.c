c983f3690258efab88a42a12bd32971eab03310d
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-20 09:10:24 -0800

    adding check for nullbyte

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
 	
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || *key == '\0' || strcmp(key, "") == 0)
 		return (0);
 
 	value_copy = strdup(value);
