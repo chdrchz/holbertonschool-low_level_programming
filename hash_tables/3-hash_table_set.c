1860a6dae44255fa7d187b85e96b92b1763754ab
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-20 09:14:35 -0800

    removing whitespace and removing uneccessary logic

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
- * hash_table_set - adds an element to a hash table 
- * @ht: hash table 
+ * hash_table_set - adds an element to a hash table
+ * @ht: hash table
  * @key: hash key
  * @value: value associated with key
  *
@@ -14,8 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *value_copy;
 	hash_node_t *current, *new_node;
 
-	if (ht == NULL || key == NULL || *key == '\0' || strcmp(key, "") == 0)
-                return (0);
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
+		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
@@ -40,6 +40,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->key = strdup(key);
 	new_node->value = value_copy;
 	new_node->next = ht->array[index];
-	ht->array[index] = new_node;	
+	ht->array[index] = new_node;
 	return (1);
 }
