eebbdd514f6937352f671794a6d267ce8642f225
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-20 07:57:03 -0800

    test

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (current == 0)
 	{
 		free(current->value);
-		current = value_copy;
+		current->value = value_copy;
 		return (1);
 	}
 
