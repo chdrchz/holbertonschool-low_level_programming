22a5c5b422baf42ab9360fc4871491957d2de5aa
Savanna Davis <saavannaa.rose@gmail.com>
2023-11-20 10:33:12 -0800

    fixing spacing

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,16 +11,21 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int iterator = 0;
 
 	new_table = malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
 		return (NULL);
+
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * new_table->size);
+
 	if (new_table->array == NULL)
 		return (NULL);
+
 	while (iterator < new_table->size)
 	{
 		iterator++;
 		new_table->array[iterator] = NULL;
 	}
+
 	return (new_table);
 }
