3eb83fc7148d8936cb18d1f0862d85fb2a3fcb0e
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-15 11:45:24 -0800

    removing print statement

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -19,7 +19,6 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			printf("Found at index: %d\n", value);
 			return (1);
 		}
 	}
