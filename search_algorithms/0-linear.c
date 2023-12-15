97a8385bf57680b7cc49c31142d032b82301e541
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-15 11:16:02 -0800

    adding linear search

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches an array for value using linear search
+ *
+ * @array: array of ints
+ * @size: size of array
+ * @value: value to search for
+ * Return: On Success: 1
+ * On Failure: -1
+ **/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			printf("Value: %d\n", value);
+	}
+	return (1);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS.H
+#define SEARCH_ALGOS.H
+
+/* HEADERS */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* PROTOTYPES */
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
