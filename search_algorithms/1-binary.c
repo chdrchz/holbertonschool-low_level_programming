4c6958f19b24065c5a11202aad49ba9b1fbf5637
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-15 12:34:57 -0800

    moving print around

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -17,24 +17,8 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	printf("Searching in array: ");
-	for (i = low; i <= high; i++)
-	{
-		printf("%d", array[i]);
-		if (i < high)
-			printf(", ");
-	}
-	printf("\n");
-
 	while (low <= high)
 	{
-		mid = low + (high - low) / 2;
-		if (array[mid] == value)
-			return (mid);
-		if (array[mid] < value)
-			low = mid + 1;
-		else
-			high = mid - 1;
 		printf("Searching in array: ");
 		for (i = low; i <= high; i++)
 		{
@@ -43,6 +27,13 @@ int binary_search(int *array, size_t size, int value)
 				printf(", ");
 		}
 		printf("\n");
+		mid = low + (high - low) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
 	}
 	return (-1);
 }
