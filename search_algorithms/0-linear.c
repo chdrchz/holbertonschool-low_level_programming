47a350db7d43f30fcf2d35f62a1807a73aa4fd0f
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-15 11:26:52 -0800

    changing values around

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -16,9 +16,9 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", array[i], value);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			printf("Found at index: %ld\n", i);
+			printf("Found at index: %ld\n", value);
 	}
 	return (1);
 }
